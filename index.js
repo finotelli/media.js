b1db4f6841627a17874e64b0470cde36814d6828
Ana Finotelli <finotelli2004@gmail.com>
2023-04-13 23:30:47 -0300

    media.js

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Some aqui a sua média bimestral!");
+
+let n1 = prompt("Digite aqui a sua primeira nota:");
+let n2 = prompt("Digite aqui a sua segunda nota:");
+let n3 = prompt("Digite aqui a sua terceira nota:");
+let n4 = prompt("Digite aqui a sua quarta nota:");
+
+resultado = (n1 + n2 + n3 + n4 / 4);
+
+alert(`Sua média bimestral é ${resultado}`)
